7f59020fd7c6d5178cd4794d3f05127525599f14
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 02:09:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence)
   reversed_words_array = []
   words_array = sentence1.split(" ")
   words_array.each {|word| reversed_words_array << word.reverse}
-  sentence_reversed_words = reversed_words_array.join(" ")
+  reversed_words_sentence = reversed_words_array.join(" ")
 end
 
 
 
-def reverse_each_word(sentence2)
+def reverse_each_word(sentence)
   reversed_words_array = []
   words_array = sentence2.split(" ")
   words_array.collect {|word| reversed_words_array << word.reverse}
-  sentence_reversed_words = reversed_words_array.join(" ")
+  reversed_words_sentence = reversed_words_array.join(" ")
 end
