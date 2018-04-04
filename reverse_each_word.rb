a1e6344784c719857089466d9d917232e3de1964
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 02:10:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reversed_words_array = []
-  words_array = sentence1.split(" ")
+  words_array = sentence.split(" ")
   words_array.each {|word| reversed_words_array << word.reverse}
   reversed_words_sentence = reversed_words_array.join(" ")
 end
@@ -9,7 +9,7 @@ end
 
 def reverse_each_word(sentence)
   reversed_words_array = []
-  words_array = sentence2.split(" ")
+  words_array = sentence.split(" ")
   words_array.collect {|word| reversed_words_array << word.reverse}
   reversed_words_sentence = reversed_words_array.join(" ")
 end
